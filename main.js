b7ba96e412f4316c4f1ba426c4d0b022b643a247
mihail.sirghi@orange.md <mr.serveen@yahoo.com>
2020-01-30 09:30:37 +0200

    [Add]: buttons & actions for different planet perpective

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 import { sun as sunModel, imageData, mercuryTexture } from "./constants.js";
 
 var scene, camera, render, container;
-let width, height;
-var light;
-width = parseInt(document.body.clientWidth);
-height = parseInt(document.body.clientHeight);
+let width = parseInt(document.body.clientWidth);
+let height = parseInt(document.body.clientHeight);
 
 container = document.createElement("div");
 document.body.appendChild(container);
@@ -12,7 +10,7 @@ document.body.appendChild(container);
 camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
 
 // Высота камеры
-camera.position.z = 8000;
+camera.position.z = 5000;
 // Поворот камеры
 camera.rotation.z = -Math.PI / 20;
 scene = new THREE.Scene();
@@ -32,10 +30,9 @@ let starsMaterial = new THREE.PointsMaterial({
   size: 1,
   sizeAttenuation: false
 });
-let stars;
 
 for (let i = 0; i < 15000; i++) {
-  var vertex = new THREE.Vector3();
+  let vertex = new THREE.Vector3();
   vertex.x = Math.random() * 2 - 1;
   vertex.y = Math.random() * 2 - 1;
   vertex.z = Math.random() * 2 - 1;
@@ -43,36 +40,34 @@ for (let i = 0; i < 15000; i++) {
   starsGeometry.vertices.push(vertex);
 }
 
-stars = new THREE.Points(starsGeometry, starsMaterial);
+let stars = new THREE.Points(starsGeometry, starsMaterial);
 scene.add(stars);
 
 // Солнце
-let texture = new THREE.TextureLoader().load(sunModel);
-var material = new THREE.MeshBasicMaterial({ map: texture });
+let texture = new THREE.TextureLoader().load("sun.jpg");
+const material = new THREE.MeshBasicMaterial({ map: texture });
 texture = new THREE.SphereGeometry(1030, 80, 50);
-var sun = new THREE.Mesh(texture, material);
+const sun = new THREE.Mesh(texture, material);
 scene.add(sun);
 
 // Земля
-var earthTexture = new THREE.TextureLoader().load(imageData);
-var material = new THREE.MeshLambertMaterial({ map: earthTexture });
+let earthTexture = new THREE.TextureLoader().load(imageData);
+const earthMaterial = new THREE.MeshLambertMaterial({ map: earthTexture });
 earthTexture = new THREE.SphereGeometry(100, 20, 20);
-var earth = new THREE.Mesh(earthTexture, material);
-// earth.castShadow = true;
+const earth = new THREE.Mesh(earthTexture, earthMaterial);
 scene.add(earth);
 
-var mercuryText = new THREE.TextureLoader().load(mercuryTexture);
-var mercuryMaterial = new THREE.MeshBasicMaterial({ map: mercuryText });
+// Меркурий
+let mercuryText = new THREE.TextureLoader().load(mercuryTexture);
+const mercuryMaterial = new THREE.MeshLambertMaterial({ map: mercuryText });
 mercuryText = new THREE.SphereGeometry(60, 20, 20);
-var mercury = new THREE.Mesh(mercuryText, mercuryMaterial);
+const mercury = new THREE.Mesh(mercuryText, mercuryMaterial);
 scene.add(mercury);
 
 // сдвиг Земли по оси Х справа
 // earth.position.x = 1000;
 
-render = window.WebGLRenderingContext
-  ? new THREE.WebGLRenderer()
-  : new THREE.CanvasRenderer();
+render = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
 render.setSize(width, height);
 container.appendChild(render.domElement);
 
@@ -81,32 +76,96 @@ var y = 0;
 animate();
 
 document.addEventListener("mousemove", e => {
-  //   y = parseInt(event.offsetY);
+  // y = parseInt(event.offsetY);
 });
 
 function animate() {
   requestAnimationFrame(animate);
-  sun.rotation.y += 0.001;
+  sun.rotation.y += 0.01;
 
-  //   2000 - радиус вращения
+  //   2500 - радиус вращения
   earth.position.x = Math.sin(t * 0.1) * 2500;
   earth.position.z = Math.cos(t * 0.1) * 2500;
 
   camera.position.y = y * 3;
-  //  смотрим на Солнце со стороны Земли
-  //   camera.position.x = earth.position.x;
-  //   camera.position.z = earth.position.z;
-
-  //   смотрим на Землю со стороны Cолнца
-  //   camera.position.x = sun.position.x;
-  //   camera.position.z = sun.position.z;
-
-  // камера следует за Землёй
-  //   camera.position.z = earth.position.z + 200;
-  //   camera.lookAt(earth.position);
-
   mercury.position.x = Math.sin(t * 0.3) * 1500;
   mercury.position.z = Math.cos(t * 0.3) * 1500;
   t += (Math.PI / 180) * 2;
   render.render(scene, camera);
+}
+
+let earthPerpectiveMode = false;
+let mercuryPerpectiveMode = false;
+let earthAnimationFrame = null;
+let mercuryAnimationFrame = null;
+
+document
+  .getElementById("earthPerspective")
+  .addEventListener("click", () => toggleEarthPerspective(earthPerpectiveMode));
+
+document
+  .getElementById("mercuryPerspective")
+  .addEventListener("click", () => toggleMercuryPerpective(mercuryPerpectiveMode));
+
+document.getElementById("resetButton").addEventListener("click", () => resetCamera());
+
+function resetCamera() {
+  cancelAnimationFrame(mercuryAnimationFrame);
+  cancelAnimationFrame(earthAnimationFrame);
+  earthPerpectiveMode = false;
+  mercuryPerpectiveMode = false;
+  camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  camera.position.z = 5000;
+  mercuryPerpectiveMode = false;
+  camera.rotation.z = -Math.PI / 20;
+  render.render(scene, camera);
+}
+
+function toggleMercuryPerpective(isEnabled) {
+  if (!isEnabled) {
+    mercuryRotationFollowingCamera();
+    mercuryPerpectiveMode = true;
+    return;
+  }
+
+  cancelAnimationFrame(mercuryAnimationFrame);
+  camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  camera.position.z = 5000;
+  mercuryPerpectiveMode = false;
+  camera.rotation.z = -Math.PI / 20;
+  render.render(scene, camera);
+  return;
+}
+
+function mercuryRotationFollowingCamera() {
+  mercuryAnimationFrame = requestAnimationFrame(mercuryRotationFollowingCamera);
+  camera.position.z = earth.position.z + 200;
+  camera.lookAt(mercury.position);
+  render.render(scene, camera);
+  return;
+}
+
+function toggleEarthPerspective(isEnabled) {
+  if (!isEnabled) {
+    earthRotationFollowingCamera();
+    earthPerpectiveMode = true;
+    mercuryPerpectiveMode = false;
+    return;
+  }
+
+  cancelAnimationFrame(earthAnimationFrame);
+  camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+  camera.position.z = 5000;
+  earthPerpectiveMode = false;
+  camera.rotation.z = -Math.PI / 20;
+  render.render(scene, camera);
+  return;
+}
+
+function earthRotationFollowingCamera() {
+  earthAnimationFrame = requestAnimationFrame(earthRotationFollowingCamera);
+  camera.position.z = earth.position.z + 200;
+  camera.lookAt(earth.position);
+  render.render(scene, camera);
+  return;
 }

ff03aa90a6519e34be1e2622c9f34228dbb80ddc
msirghi <mr.serven@yahoo.com>
2020-02-15 19:57:21 +0200

    [Add]: sun glow & space

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-import { sun as sunModel, imageData, mercuryImage, moonImage } from "./constants.js";
+import {
+  sun as sunModel,
+  imageData,
+  mercuryImage,
+  moonImage
+} from "./constants.js";
 
 let scene, camera, render, container;
 let width, height;
@@ -8,7 +13,7 @@ height = parseInt(document.body.clientHeight);
 container = document.createElement("div");
 document.body.appendChild(container);
 
-camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+camera = new THREE.PerspectiveCamera(95, width / height, 1, 10000);
 
 // Высота камеры
 camera.position.z = 8000;
@@ -26,13 +31,13 @@ scene.add(light);
 let starsGeometry = new THREE.Geometry();
 let starsMaterial = new THREE.PointsMaterial({
   color: 0xbbbbbb,
-  opacity: 0.6,
+  opacity: 0.3,
   size: 1,
   sizeAttenuation: false
 });
 let stars;
 
-for (let i = 0; i < 15000; i++) {
+for (let i = 0; i < 5000; i++) {
   let vertex = new THREE.Vector3();
   vertex.x = Math.random() * 2 - 1;
   vertex.y = Math.random() * 2 - 1;
@@ -49,6 +54,17 @@ let sunTexture = new THREE.TextureLoader().load(sunModel);
 let sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
 sunTexture = new THREE.SphereGeometry(1030, 80, 50);
 let sun = new THREE.Mesh(sunTexture, sunMaterial);
+
+var sunGlow1Sphere = new THREE.SphereGeometry(1100, 80, 50);
+var sunGlow1Material = new THREE.MeshBasicMaterial({
+  transparent: true,
+  opacity: 0.2
+});
+sunGlow1Material.color = new THREE.Color("rgb(255, 190, 0)");
+
+var sunGlow1 = new THREE.Mesh(sunGlow1Sphere, sunGlow1Material);
+sunGlow1.position.set(0, 0, 0);
+sun.add(sunGlow1);
 scene.add(sun);
 
 // Земля
@@ -56,7 +72,6 @@ let earthTexture = new THREE.TextureLoader().load(imageData);
 let earthMaterial = new THREE.MeshLambertMaterial({ map: earthTexture });
 earthTexture = new THREE.SphereGeometry(100, 20, 20);
 let earth = new THREE.Mesh(earthTexture, earthMaterial);
-// earth.castShadow = true;
 scene.add(earth);
 
 let mercuryTexture = new THREE.TextureLoader().load(mercuryImage);
@@ -71,17 +86,44 @@ moonTexture = new THREE.SphereGeometry(60, 20, 20);
 let moon = new THREE.Mesh(moonTexture, moonMaterial);
 scene.add(moon);
 
-// сдвиг Земли по оси Х справа
-// earth.position.x = 1000;
+let skybox_group = new THREE.Object3D();
+let SkyboxMesh = CreateSphere("eso_dark.jpg", 6500, 0, "Skybox", true);
+SkyboxMesh.material.side = THREE.BackSide;
+SkyboxMesh.rotation.x = (Math.PI / 180) * 63;
+skybox_group.add(SkyboxMesh);
 
-render = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
+scene.add(skybox_group);
+
+function CreateSphere(texture_u, radius, polygon_count, name, basic) {
+  let manager = new THREE.LoadingManager();
+  let sphere_loader = new THREE.TextureLoader(manager);
+  let sphere_texture = sphere_loader.load(texture_u);
+  let sphere_geometry = new THREE.SphereGeometry(
+    radius,
+    polygon_count,
+    polygon_count
+  );
+  if (basic == true) {
+    var sphere_material = new THREE.MeshBasicMaterial({ map: sphere_texture });
+  } else {
+    var sphere_material = new THREE.MeshLambertMaterial({
+      map: sphere_texture
+    });
+  }
+  let sphere_mesh = new THREE.Mesh(sphere_geometry, sphere_material);
+  sphere_mesh.name = name;
+  return sphere_mesh;
+}
+
+render = window.WebGLRenderingContext
+  ? new THREE.WebGLRenderer()
+  : new THREE.CanvasRenderer();
 render.setSize(width, height);
 container.appendChild(render.domElement);
 
 let controls = new THREE.OrbitControls(camera, render.domElement);
 
 let t = 0;
-let y = 0;
 animate();
 
 // document.addEventListener("mousemove", e => {
@@ -95,6 +137,8 @@ function animate() {
   //   2000 - радиус вращения
   earth.position.x = Math.sin(t * 0.1) * 2500;
   earth.position.z = Math.cos(t * 0.1) * 2500;
+
+  sun.rotation.y += 0.002;
 
   moon.position.x = earth.position.x + Math.sin(t * 0.5) * 500;
   moon.position.z = earth.position.z + Math.cos(t * 0.5) * 500;
@@ -117,9 +161,13 @@ document
 
 document
   .getElementById("mercuryPerspective")
-  .addEventListener("click", () => toggleMercuryPerpective(mercuryPerpectiveMode));
+  .addEventListener("click", () =>
+    toggleMercuryPerpective(mercuryPerpectiveMode)
+  );
 
-document.getElementById("resetButton").addEventListener("click", () => resetCamera());
+document
+  .getElementById("resetButton")
+  .addEventListener("click", () => resetCamera());
 
 function resetCamera() {
   //   cancelAnimationFrame(mercuryAnimationFrame);
